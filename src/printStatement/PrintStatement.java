aaf25e9f64402517fe381feb5f4e00b7b4261371
ravind0303 <ravind.pcf@gamil.com>
2022-10-30 16:41:16 +0530

    Test variable

---
+++ src/printStatement/PrintStatement.java
@@ -0,0 +1,28 @@
+package printStatement;
+
+public class PrintStatement {
+
+    public static void main(String[] args) {
+
+        //We are using String value
+        System.out.println("This is Print statement");
+        System.out.println("This is java class");
+        System.out.println("Hello Sonu Kumar");
+        System.out.print("Hello India   ");
+        System.out.println("My name is ravind Kumar");
+
+        //we are using Integer value
+        System.out.println(23);
+        System.out.println(65);
+        System.out.println(2+5);
+        System.out.println(4*5);
+        System.out.println(30/3);
+        System.out.println(54-23);
+
+        //We are using Float
+
+        System.out.println(39.85f);
+        System.out.println(27.68f);
+        System.out.println(53.45 );
+    }
+}
---
+++ src/variables/Variables.java
@@ -0,0 +1,46 @@
+package variables;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+//        In Java, there are different types of variables, for example:
+//        String - stores text, such as "Hello". String values are surrounded by double quotes
+//        int - stores integers (whole numbers), without decimals, such as 123 or -123
+//        float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+//        char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+//        boolean - stores values with two states: true or false
+
+        //String
+        String s2;
+
+        s2="Garima";
+        System.out.println(s2);
+
+        //Integer
+       // int age=40;
+
+        int myNum;
+        myNum = 40;
+        System.out.println(myNum);
+
+        //Float
+       // float area=49.80f;
+        float area1;
+        area1=49.80f;
+        System.out.println(area1);
+
+        //Charactor
+        char a;
+        a='A';
+        System.out.println(a);
+
+
+        //Boolaen
+
+        boolean color;
+        color=true;
+        System.out.println("Color white is "+color);
+    }
+
+}
